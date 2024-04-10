577f0b3511d93fcdaa6aa1c16678c648ad2d1b1e
Pearl Design <pearldesign06@gmail.com>
2024-04-10 23:48:33 +0200

    rajout du plugin sass live compiler dans vscode ensuite rajout des images dans les assets avec le fichier video. Rajout de la nouvelle section

--- wp-config.php
+++ wp-config.php
@@ -21,6 +21,8 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', 'C:\MAMP\htdocs\Projet-9-Koukaki\wp-content\plugins\wp-super-cache/' );
 define( 'DB_NAME', 'koukaki' );
 
 /** Utilisateur de la base de données MySQL. */
