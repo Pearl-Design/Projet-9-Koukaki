fb0375e6325119b89ebc7f4d05b43d773fcda09c
Pearl Design <pearldesign06@gmail.com>
2024-04-11 18:04:45 +0200

    début d'animation sur les sections

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -59,14 +59,7 @@ get_header();
       <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
     </div>
   </section>
-  <section class="nomination">
-  <div>
-    <h2 class="nomination__title">Fleurs d’oranger &amp; chats errants<br>
-      est nominé aux Oscars Short<br>
-      Film Animated de 2022 !</h2>
-  </div>
-  <div class="nomination__logo"><img src="<?php echo get_theme_file_uri() . '/assets/images/oscars-short-film-animated.png'; ?>" alt="Oscars short film animated"> </div>
-</section>
+  <?php get_template_part('template-parts/nomination'); ?>
 </main>
 <!-- #main -->
 
