39a0756e9f69a9ca77005216547ab8f76f4632cf
Pearl Design <pearldesign06@gmail.com>
2024-04-11 22:50:30 +0200

    animation titre terminé

--- wp-content/themes/foce-child/js/animationJs.js
+++ wp-content/themes/foce-child/js/animationJs.js
@@ -1,16 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.story, #studio, .nomination');
+    const sections = document.querySelectorAll('.story, #studio, .nomination');
 
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('animate');
-        observer.unobserve(entry.target);
-      }
+    sections.forEach(section => {
+        const title = section.querySelector('h2');
+        if (title && !section.classList.contains('nomination')) {
+            convertToSpanStructure(title);
+        }
     });
-  }, { threshold: 0.5 });
 
-  sections.forEach(section => {
-    observer.observe(section);
-  });
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animateSection');
+                setTimeout(() => {
+                    animateTitle(entry.target);
+                }, 1500); // Delay set to 3 seconds
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+    function convertToSpanStructure(title) {
+        const words = title.textContent.split(' ');
+        title.innerHTML = '';
+
+        words.forEach((word) => {
+            const span = document.createElement('span');
+            span.textContent = word;
+            title.appendChild(span);
+        });
+    }
+
+    function animateTitle(section) {
+        const title = section.querySelector('h2');
+
+        if (!title) return;
+
+        const spans = title.querySelectorAll('span');
+        spans.forEach((span, index) => {
+            setTimeout(() => {
+                span.classList.add('animateTitle');
+            }, index * 30); // Adjust the delay here as needed
+        });
+    }
 });
