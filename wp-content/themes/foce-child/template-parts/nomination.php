8e09eca5244483d427204e50ef80e94081ed5a7c
Pearl Design <pearldesign06@gmail.com>
2024-04-11 21:12:01 +0200

    fin de la scss de la nouvelle section avec le rajout des fleurs

--- wp-content/themes/foce-child/template-parts/nomination.php
+++ wp-content/themes/foce-child/template-parts/nomination.php
@@ -1,8 +1,6 @@
 <section class="nomination">
-  <div>
-    <h2 class="nomination__title">Fleurs d’oranger &amp; chats errants<br>
+    <h2>Fleurs d’oranger &amp; chats errants<br>
       est nominé aux Oscars Short<br>
       Film Animated de 2022 !</h2>
-  </div>
-  <div class="nomination__logo"><img src="<?php echo get_theme_file_uri() . '/assets/images/oscars-short-film-animated.png'; ?>" alt="Oscars short film animated"> </div>
+  <img src="<?php echo get_theme_file_uri() . '/assets/images/oscars-short-film-animated.png'; ?>" alt="Oscars short film animated">
 </section>
